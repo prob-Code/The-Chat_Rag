79c769443082c86bb338625fdfc34d6c0209bafa
Ojas Satdeve <ojassatdeve@gamil.com>
2026-03-20 18:13:19 +0530

    remove large/cached files

--- lightrag/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LightRAG - Graph-Enhanced Retrieval Augmented Generation
-A multi-faith, clarity-oriented Decision Support System
-"""
-
-from .config import LightRAGConfig
-
-__version__ = "1.0.0"
-__all__ = ["LightRAGConfig"]
